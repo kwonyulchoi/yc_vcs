410f45c28c1304461e1c82497dc978a1f63801a8
user <user@I404_165>
2017-12-07 15:46:50 +0900

    Sungwoo

--- Sungwoo/WebClient/lib/src/main/java/com/example/lib/StudentApi.java
+++
@@ -1,9 +0,0 @@
-package com.example.lib;
-
-import java.util.List;
-
-public interface StudentApi {
-	public List<Student> getStudents();
-	public Student PostStudent(Student student);
-	public Boolean deleteStudent(Integer id);
-}
